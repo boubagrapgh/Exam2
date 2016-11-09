62b74492108c08feab47319310d3d297348770ad
bonneau <boubagraph@gmail.com>
2016-11-09 15:19:19 +0100

    exaM2

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 (function(){
 
+var app = angular.module('app', []);
+app.controller('appController', function(){
+	this.personne = "object";
+});
 
+/*app.controller('homeController', function(){
+
+});*/
+
+app.directive('home',
+return{
+	restrict : 'A',
+	templateUrl : '/partials/home/home.html',
+});
 
 
 
